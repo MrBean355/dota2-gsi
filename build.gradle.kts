ec83c63b440f857501a4031c5c2824ce71779c6b
Mike Johnston <mrbean355@gmail.com>
2020-10-11 17:21:50 +0200

    Implement snapshot publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    `maven-publish`
 }
 
 group = "com.github.mrbean355"
-version = "1.0-SNAPSHOT"
+ext {
+    set("artifactId", "dota2-gsi")
+}
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -18,4 +22,26 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("SONATYPE_NEXUS_USERNAME") ?: ""
+                password = System.getenv("SONATYPE_NEXUS_PASSWORD") ?: ""
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = extra["artifactId"].toString()
+            from(components["java"])
+        }
+    }
 }
