6fa7eaf3ce7fd49c3f4b1ac0d79372485ccf2588
Mike Johnston <mrbean355@gmail.com>
2022-08-08 23:54:02 +0200

    Prepare for development of 2.2.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "com.github.mrbean355"
 val artifactId by extra("dota2-gsi")
-version = "2.1.0"
+version = "2.2.0-SNAPSHOT"
 
 dependencies {
     implementation(libs.ktor.server.netty)
